505c6eaf64d35f010ad402f45d01283e7dd99dc1
kazaki0806 <92520218+kazaki0806@users.noreply.github.com>
2022-08-21 09:12:26 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '贫弱！贫弱！';
-    if (score <= 99) return '不做人啦';
-    if (score <= 149) return '真的是嗨到不行';
-    if (score <= 199) return '您';
+    if (score <= 49) return '好好上网课！';
+    if (score <= 99) return '还是卷子做得少！';
+    if (score <= 149) return '体育课别想上了！';
+    if (score <= 199) return '是不是不想放假啦！！！';
     return '人？';
 }
 
